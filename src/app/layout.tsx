8d10bcc3b5730f4d357f6a9cbf9f11fc4b586a11
Himanshu1198 <himanshumain97@gmail.com>
2024-07-16 11:13:32 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,34 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='dark'>
-      <head>
-        {/* Set the title dynamically using Next.js Head component */}
-        <title>{metadata.title as string}</title>
-
-        {/* Optionally, include other meta tags, stylesheets, etc. */}
-        <meta name='description' content={metadata.description as string} />
-
-        {/* Include global styles */}
-        <link rel='stylesheet' href='/path/to/globals.css' />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/site.webmanifest'/>
-      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
